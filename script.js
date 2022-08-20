050bf4f533998131a9f737413b2b502d9e3d077a
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2022-08-20 19:26:36 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let NI = 0;
+
+let txtnumber = document.getElementById("number");
+
+function soma(){
+    NI += 1;
+
+    txtnumber.innerHTML = NI;
+}
+
+function subtracao(){
+    NI -= 1;
+
+    if (NI < 0){
+         NI = 0
+    }
+
+    txtnumber.innerHTML = NI;
+
+    
+}
+
+function reset(){
+    NI = 0
+    txtnumber.innerHTML = NI;
+}
